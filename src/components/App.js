1494f0d3f034f5967b033f98b3f57cb72b40e0e5
Daria Approvato <dariaapprovato@gmail.com>
2022-12-07 16:24:58 -0500

    commit tree 1b7e3ecb27f460ab5e8f3e4ca2fa27092e07131b

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body:
+    {
+      "prompt": string,
+      "answers": array of string,
+      "correctIndex": integer
+    }
+  })
+    .then((r) => r.json())
+    .then((newItem) => console.log(newItem));
+})
 
   return (
     <main>
--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -20,6 +20,20 @@ function QuestionForm(props) {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
+    fetch("http://localhost:4000/questions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body:
+    {
+      "prompt": formData.prompt,
+      "answers": [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+      "correctIndex": formData.correctIndex
+    }
+  })
+    .then((r) => r.json())
+    .then((newItem) => console.log(newItem));
   }
 
   return (
